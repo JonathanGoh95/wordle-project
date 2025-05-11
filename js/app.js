2595bd408ad906c30ffb7adeff20bbaba38620b9
Jonathan Goh <jonathangohmh@gmail.com>
2025-05-11 14:32:49 +0800

    Functionality Updates

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 //Module Imports
-import { wordsToGuess } from "./wordsToGuess";
+import { wordsToGuess } from "./wordsToGuess.js";
 
 //Declaration of Variables
+const headerTitle = document.querySelector("h1");
 const numberOfGuesses = 6;
 let remainingGuesses = numberOfGuesses;
 let currentGuess = []; //Array for holding the current word by user
 let nextLetter = 0;
+let gameMsg = document.createElement("p");
 let selectedWord =
   wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)]; //Selects a random word to start playing
 console.log(selectedWord);
@@ -28,5 +30,170 @@ const main = () => {
     gameBoard.appendChild(rowOfLetters);
   }
 };
+
+//Input from User Keyboard
+document.addEventListener("keyup", (event) => {
+  if (remainingGuesses === 0) {
+    return;
+  }
+
+  //Remove letter appendChild clicking 'Backspace'
+  let userInput = String(event.key);
+  if (userInput === "Backspace" && nextLetter !== 0) {
+    deleteLetter();
+    return;
+  }
+
+  //Check if the answer is correct each time 'Enter' is clicked/pressed
+  if (userInput === "Enter") {
+    checkAnswer();
+    // gameMsg.remove();
+    return;
+  }
+
+  let found = userInput.match(/[a-z]/gi);
+  if (!found || found.length > 1) {
+    //If condition doesn't match alphabetical character or contain multiple characters, skip/ignore event.
+    return;
+  } else {
+    insertLetter(userInput);
+  }
+});
+
+const insertLetter = (userInput) => {
+  //When the word limit is reached
+  if (nextLetter === 5) {
+    return;
+  }
+  userInput = userInput.toLowerCase();
+
+  let row =
+    document.getElementsByClassName("rowOfLetters")[6 - remainingGuesses];
+  let box = row.children[nextLetter];
+  box.textContent = userInput;
+  box.classList.add("filledBox");
+  currentGuess.push(userInput);
+  nextLetter++;
+};
+
+const deleteLetter = () => {
+  let row =
+    document.getElementsByClassName("rowOfLetters")[6 - remainingGuesses];
+  let box = row.children[nextLetter - 1];
+  box.textContent = "";
+  box.classList.remove("filledBox");
+  currentGuess.pop();
+  nextLetter--;
+};
+
+const checkAnswer = () => {
+  let row =
+    document.getElementsByClassName("rowOfLetters")[6 - remainingGuesses];
+  let guessString = "";
+  let rightGuess = [...selectedWord];
+  //Append the user input to an empty string for checking
+  for (const val of currentGuess) {
+    guessString += val;
+  }
+
+  if (guessString.length !== 5) {
+    gameMsg.textContent = "Not enough letters!";
+    headerTitle.appendChild(gameMsg);
+    // alert("Not enough letters!");
+    return;
+  }
+
+  if (!wordsToGuess.includes(guessString)) {
+    gameMsg.textContent = "Word does not exist in list!";
+    headerTitle.appendChild(gameMsg);
+    // alert("Word not in list!");
+    return;
+  }
+
+  for (let i = 0; i < 5; i++) {
+    let letterColor = "";
+    let box = row.children[i];
+    let letter = currentGuess[i];
+
+    let letterPosition = rightGuess.indexOf(currentGuess[i]);
+    // Check if letter is in the correct position
+    if (letterPosition === -1) {
+      letterColor = "grey";
+    } else {
+      if (currentGuess[i] === rightGuess[i]) {
+        // Shades the letter box green if letter is in the correct position
+        letterColor = "green";
+      } else {
+        // Shades the letter box yellow if letter is in the word but in the wrong position
+        letterColor = "yellow";
+      }
+
+      rightGuess[letterPosition] = "#";
+    }
+    //Shade Animation
+    let delay = 500 * i;
+    setTimeout(() => {
+      //shade box
+      box.style.backgroundColor = letterColor;
+      shadeKeyboard(letter, letterColor);
+    }, delay);
+  }
+
+  if (guessString === selectedWord) {
+    gameMsg.textContent =
+      "Congrats! You guessed the word correctly! Game Over!";
+    headerTitle.appendChild(gameMsg);
+    // alert("You guessed right! Game over!");
+    remainingGuesses = 0;
+    return;
+  } else {
+    remainingGuesses--;
+    currentGuess = [];
+    nextLetter = 0;
+
+    if (remainingGuesses === 0) {
+      gameMsg.textContent = `You have ran out of guesses! Game Over!
+      The right word was: "${selectedWord}`;
+      headerTitle.appendChild(gameMsg);
+      //   alert("You've run out of guesses! Game over!");
+      //   alert(`The right word was: "${selectedWord}"`);
+    }
+  }
+};
+
+const shadeKeyboard = (letter, color) => {
+  for (const elem of document.getElementsByClassName("keyboardButton")) {
+    if (elem.textContent === letter) {
+      let oldColor = elem.style.backgroundColor;
+      //Do nothing if key is already green
+      if (oldColor === "green") {
+        return;
+      }
+      //Same as above but for yellow
+      if (oldColor === "yellow" && color !== "green") {
+        return;
+      }
+      //Sets the background color to grey if both conditions above are not met
+      elem.style.backgroundColor = color;
+      break;
+    }
+  }
+};
+
+//Input from On-Screen Keyboard
+document.getElementById("keyboardLayout").addEventListener("click", (event) => {
+  const target = event.target;
+
+  if (!target.classList.contains("keyboardButton")) {
+    return;
+  }
+  let key = target.textContent;
+
+  if (key === "Del") {
+    key = "Backspace";
+  }
+
+  document.dispatchEvent(new KeyboardEvent("keyup", { key: key }));
+});
 
 main();
